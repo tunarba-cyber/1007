ecb318a9c9a4edbec74d48596ab07a023c4b0a44
tunarba-cyber <tunarba@code.edu.az>
2026-03-24 12:17:48 +0400

    task

---
+++ Task_99922/Task_99922/Program.cs
@@ -0,0 +1,57 @@
+﻿
+//task1
+//byte a = 3;
+//byte b = 15;
+//byte c = 9;
+//if (a >= b && a >= c) 
+//{
+//    Console.WriteLine(a);
+//}
+//else if (b>=a && b>=c)
+//{
+//    Console.WriteLine(b);
+//}
+//else
+//{
+//    Console.WriteLine(c);
+//}
+
+
+
+
+
+
+
+//task2
+//int num = 97;
+//if (num >= 0 && num < 65)
+//{
+//    Console.WriteLine("kesilib");
+//}
+//else if (num >= 65 && num < 85)
+//{
+//    Console.WriteLine("Adi diplom alib");
+//}
+//else if (num>=85 && num<95)
+//{
+//    Console.WriteLine("seref diplomu alib");
+//}
+//else if (num>=95 && num<100)
+//{
+//    Console.WriteLine("yuksek seref diplomu alib");
+//}
+//else
+//{
+//    Console.WriteLine("daxil etdiyiniz netice sehvdir");
+//}
+
+
+
+
+
+
+//task3
+//int a = 56;
+//int b = a % 10;
+//int c = (a - b) / 10;
+//Console.WriteLine(b + c);
